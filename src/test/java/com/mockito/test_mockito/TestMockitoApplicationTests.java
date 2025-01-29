d29d5b05d58060df9e3addff1b136a7bb9c5545d
Евгений <Safronovbr@mail.ru>
2025-01-29 15:24:34 +0300

    Start

---
+++ src/test/java/com/mockito/test_mockito/TestMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockito.test_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
